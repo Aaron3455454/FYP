5ac21efc8e5dfdcb3ec31740d969949607534a70
aarond6504@gmail.com <aarond6504@gmail.com>
2024-11-21 18:49:28 +0000

    Minor changes

--- main.py
+++ main.py
@@ -2,5 +2,7 @@ import matplotlib as plt
 import numpy as np
 import pandas as pd
 
-df = pd.read_csv("Datasets2\diabetes.csv")
-print(df.head(100))
+df = pd.read_csv("Datasets2\PrimaIndiansDataset.csv")
+print(df.tail())
+print(df.info())
+print(df.describe())
